1a181af7ac2d6b70c0795651a2a834b732280d59
Spencer Morris <spencermorris22@gmail.com>
2015-08-10 15:16:22 -0500

    Fixed photo issue with position player form.

--- Arm_Age_Finder/PositionPlayerForm.Designer.cs
+++ Arm_Age_Finder/PositionPlayerForm.Designer.cs
@@ -109,7 +109,7 @@ namespace Arm_Age_Finder
             this.pitcherFormTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
             this.pitcherFormTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
             this.pitcherFormTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.pitcherFormTableLayout.Size = new System.Drawing.Size(1181, 837);
+            this.pitcherFormTableLayout.Size = new System.Drawing.Size(1181, 733);
             this.pitcherFormTableLayout.TabIndex = 10;
             // 
             // PP_Question7TextBox
@@ -117,7 +117,7 @@ namespace Arm_Age_Finder
             this.PP_Question7TextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.PP_Question7TextBox.BackColor = System.Drawing.Color.Gold;
             this.PP_Question7TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PP_Question7TextBox.Location = new System.Drawing.Point(983, 670);
+            this.PP_Question7TextBox.Location = new System.Drawing.Point(983, 587);
             this.PP_Question7TextBox.Name = "PP_Question7TextBox";
             this.PP_Question7TextBox.Size = new System.Drawing.Size(100, 40);
             this.PP_Question7TextBox.TabIndex = 15;
@@ -128,7 +128,7 @@ namespace Arm_Age_Finder
             this.PP_Question6TextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.PP_Question6TextBox.BackColor = System.Drawing.Color.Gold;
             this.PP_Question6TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PP_Question6TextBox.Location = new System.Drawing.Point(983, 578);
+            this.PP_Question6TextBox.Location = new System.Drawing.Point(983, 506);
             this.PP_Question6TextBox.Name = "PP_Question6TextBox";
             this.PP_Question6TextBox.Size = new System.Drawing.Size(100, 40);
             this.PP_Question6TextBox.TabIndex = 14;
@@ -139,7 +139,7 @@ namespace Arm_Age_Finder
             this.PP_Question5TextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.PP_Question5TextBox.BackColor = System.Drawing.Color.Gold;
             this.PP_Question5TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PP_Question5TextBox.Location = new System.Drawing.Point(983, 486);
+            this.PP_Question5TextBox.Location = new System.Drawing.Point(983, 425);
             this.PP_Question5TextBox.Name = "PP_Question5TextBox";
             this.PP_Question5TextBox.Size = new System.Drawing.Size(100, 40);
             this.PP_Question5TextBox.TabIndex = 13;
@@ -150,7 +150,7 @@ namespace Arm_Age_Finder
             this.PP_Question4TextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.PP_Question4TextBox.BackColor = System.Drawing.Color.Gold;
             this.PP_Question4TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PP_Question4TextBox.Location = new System.Drawing.Point(983, 394);
+            this.PP_Question4TextBox.Location = new System.Drawing.Point(983, 344);
             this.PP_Question4TextBox.Name = "PP_Question4TextBox";
             this.PP_Question4TextBox.Size = new System.Drawing.Size(100, 40);
             this.PP_Question4TextBox.TabIndex = 12;
@@ -161,7 +161,7 @@ namespace Arm_Age_Finder
             this.PP_Question3TextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.PP_Question3TextBox.BackColor = System.Drawing.Color.Gold;
             this.PP_Question3TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PP_Question3TextBox.Location = new System.Drawing.Point(983, 302);
+            this.PP_Question3TextBox.Location = new System.Drawing.Point(983, 263);
             this.PP_Question3TextBox.Name = "PP_Question3TextBox";
             this.PP_Question3TextBox.Size = new System.Drawing.Size(100, 40);
             this.PP_Question3TextBox.TabIndex = 11;
@@ -172,7 +172,7 @@ namespace Arm_Age_Finder
             this.PP_Question2TextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.PP_Question2TextBox.BackColor = System.Drawing.Color.Gold;
             this.PP_Question2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PP_Question2TextBox.Location = new System.Drawing.Point(983, 210);
+            this.PP_Question2TextBox.Location = new System.Drawing.Point(983, 182);
             this.PP_Question2TextBox.Name = "PP_Question2TextBox";
             this.PP_Question2TextBox.Size = new System.Drawing.Size(100, 40);
             this.PP_Question2TextBox.TabIndex = 10;
@@ -186,10 +186,10 @@ namespace Arm_Age_Finder
             this.PP_Question6.AutoSize = true;
             this.PP_Question6.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PP_Question6.ForeColor = System.Drawing.Color.Gold;
-            this.PP_Question6.Location = new System.Drawing.Point(3, 552);
+            this.PP_Question6.Location = new System.Drawing.Point(3, 486);
             this.PP_Question6.Name = "PP_Question6";
             this.PP_Question6.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.PP_Question6.Size = new System.Drawing.Size(879, 92);
+            this.PP_Question6.Size = new System.Drawing.Size(879, 81);
             this.PP_Question6.TabIndex = 5;
             this.PP_Question6.Text = "Have you ever pitched before? (Yes/No)";
             this.PP_Question6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -202,10 +202,10 @@ namespace Arm_Age_Finder
             this.PP_Question5.AutoSize = true;
             this.PP_Question5.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PP_Question5.ForeColor = System.Drawing.Color.Gold;
-            this.PP_Question5.Location = new System.Drawing.Point(3, 460);
+            this.PP_Question5.Location = new System.Drawing.Point(3, 405);
             this.PP_Question5.Name = "PP_Question5";
             this.PP_Question5.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.PP_Question5.Size = new System.Drawing.Size(879, 92);
+            this.PP_Question5.Size = new System.Drawing.Size(879, 81);
             this.PP_Question5.TabIndex = 4;
             this.PP_Question5.Text = "How many days a week do you play catch?";
             this.PP_Question5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -218,10 +218,10 @@ namespace Arm_Age_Finder
             this.PP_Question4.AutoSize = true;
             this.PP_Question4.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PP_Question4.ForeColor = System.Drawing.Color.Gold;
-            this.PP_Question4.Location = new System.Drawing.Point(3, 368);
+            this.PP_Question4.Location = new System.Drawing.Point(3, 324);
             this.PP_Question4.Name = "PP_Question4";
             this.PP_Question4.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.PP_Question4.Size = new System.Drawing.Size(879, 92);
+            this.PP_Question4.Size = new System.Drawing.Size(879, 81);
             this.PP_Question4.TabIndex = 3;
             this.PP_Question4.Text = "About how many plays are you apart of each game?";
             this.PP_Question4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -234,11 +234,11 @@ namespace Arm_Age_Finder
             this.PP_Question3.AutoSize = true;
             this.PP_Question3.Font = new System.Drawing.Font("Segoe Script", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PP_Question3.ForeColor = System.Drawing.Color.Gold;
-            this.PP_Question3.Location = new System.Drawing.Point(3, 276);
+            this.PP_Question3.Location = new System.Drawing.Point(3, 243);
             this.PP_Question3.Name = "PP_Question3";
             this.PP_Question3.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
             this.PP_Question3.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.PP_Question3.Size = new System.Drawing.Size(879, 92);
+            this.PP_Question3.Size = new System.Drawing.Size(879, 81);
             this.PP_Question3.TabIndex = 2;
             this.PP_Question3.Text = "How many years have you been playing this position?";
             this.PP_Question3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -251,14 +251,13 @@ namespace Arm_Age_Finder
             this.PP_Question2.AutoSize = true;
             this.PP_Question2.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PP_Question2.ForeColor = System.Drawing.Color.Gold;
-            this.PP_Question2.Location = new System.Drawing.Point(3, 184);
+            this.PP_Question2.Location = new System.Drawing.Point(3, 162);
             this.PP_Question2.Name = "PP_Question2";
             this.PP_Question2.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.PP_Question2.Size = new System.Drawing.Size(879, 92);
+            this.PP_Question2.Size = new System.Drawing.Size(879, 81);
             this.PP_Question2.TabIndex = 1;
             this.PP_Question2.Text = "What position do you primarily play? (1-9)";
             this.PP_Question2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.PP_Question2.Click += new System.EventHandler(this.PP_Question2_Click);
             // 
             // PP_Question1
             // 
@@ -268,10 +267,10 @@ namespace Arm_Age_Finder
             this.PP_Question1.AutoSize = true;
             this.PP_Question1.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PP_Question1.ForeColor = System.Drawing.Color.Gold;
-            this.PP_Question1.Location = new System.Drawing.Point(3, 92);
+            this.PP_Question1.Location = new System.Drawing.Point(3, 81);
             this.PP_Question1.Name = "PP_Question1";
             this.PP_Question1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.PP_Question1.Size = new System.Drawing.Size(879, 92);
+            this.PP_Question1.Size = new System.Drawing.Size(879, 81);
             this.PP_Question1.TabIndex = 0;
             this.PP_Question1.Text = "How old are you?";
             this.PP_Question1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -287,7 +286,7 @@ namespace Arm_Age_Finder
             this.PP_HeaderLabel.ForeColor = System.Drawing.Color.Gold;
             this.PP_HeaderLabel.Location = new System.Drawing.Point(3, 0);
             this.PP_HeaderLabel.Name = "PP_HeaderLabel";
-            this.PP_HeaderLabel.Size = new System.Drawing.Size(1175, 92);
+            this.PP_HeaderLabel.Size = new System.Drawing.Size(1175, 81);
             this.PP_HeaderLabel.TabIndex = 8;
             this.PP_HeaderLabel.Text = "Answer the following questions:";
             this.PP_HeaderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -297,7 +296,7 @@ namespace Arm_Age_Finder
             this.PP_Question1TextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.PP_Question1TextBox.BackColor = System.Drawing.Color.Gold;
             this.PP_Question1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PP_Question1TextBox.Location = new System.Drawing.Point(983, 118);
+            this.PP_Question1TextBox.Location = new System.Drawing.Point(983, 101);
             this.PP_Question1TextBox.Name = "PP_Question1TextBox";
             this.PP_Question1TextBox.Size = new System.Drawing.Size(100, 40);
             this.PP_Question1TextBox.TabIndex = 9;
@@ -309,7 +308,7 @@ namespace Arm_Age_Finder
             this.PP_CalculateButton.BackColor = System.Drawing.Color.Gold;
             this.pitcherFormTableLayout.SetColumnSpan(this.PP_CalculateButton, 2);
             this.PP_CalculateButton.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PP_CalculateButton.Location = new System.Drawing.Point(536, 764);
+            this.PP_CalculateButton.Location = new System.Drawing.Point(536, 668);
             this.PP_CalculateButton.Name = "PP_CalculateButton";
             this.PP_CalculateButton.Size = new System.Drawing.Size(108, 44);
             this.PP_CalculateButton.TabIndex = 18;
@@ -324,10 +323,10 @@ namespace Arm_Age_Finder
             this.PP_Question7.AutoSize = true;
             this.PP_Question7.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PP_Question7.ForeColor = System.Drawing.Color.Gold;
-            this.PP_Question7.Location = new System.Drawing.Point(3, 644);
+            this.PP_Question7.Location = new System.Drawing.Point(3, 567);
             this.PP_Question7.Name = "PP_Question7";
             this.PP_Question7.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.PP_Question7.Size = new System.Drawing.Size(879, 92);
+            this.PP_Question7.Size = new System.Drawing.Size(879, 81);
             this.PP_Question7.TabIndex = 7;
             this.PP_Question7.Text = "How many serious arm injuries have you had?";
             this.PP_Question7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -338,11 +337,10 @@ namespace Arm_Age_Finder
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1181, 837);
+            this.ClientSize = new System.Drawing.Size(1181, 733);
             this.Controls.Add(this.pitcherFormTableLayout);
             this.Controls.Add(this.headerLabel);
             this.Controls.Add(this.label1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PositionPlayerForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Position Player";
