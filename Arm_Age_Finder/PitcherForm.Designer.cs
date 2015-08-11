8cc4cd55445a4164269b1731886663721ebceb8a
Spencer Morris <spencermorris22@gmail.com>
2015-08-11 12:16:49 -0500

    Fixed some formatting

--- Arm_Age_Finder/PitcherForm.Designer.cs
+++ Arm_Age_Finder/PitcherForm.Designer.cs
@@ -320,12 +320,13 @@
             this.P_Question7.AutoSize = true;
             this.P_Question7.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.P_Question7.ForeColor = System.Drawing.Color.Gold;
+            this.P_Question7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.P_Question7.Location = new System.Drawing.Point(3, 504);
             this.P_Question7.Name = "P_Question7";
             this.P_Question7.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
             this.P_Question7.Size = new System.Drawing.Size(858, 72);
             this.P_Question7.TabIndex = 7;
-            this.P_Question7.Text = "Have you played a position other than pitcher before? (Yes/No)";
+            this.P_Question7.Text = "Have you played a position in the field before? (Yes/No)";
             this.P_Question7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // P_Question8
